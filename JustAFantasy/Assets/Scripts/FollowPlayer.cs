ad0063ac5c9f918f6d939ac75860e31941029456
davide9 <bianchi_davide@hotmail.it>
2014-11-20 13:47:16 +0100

    Aggiunto script inseguimento camera-player

---
+++ JustAFantasy/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public GameObject follower;
+	public float speed = 2f;
+	public Vector2 offset = new Vector2(0,0);
+	public float tollerance = 0.5f;
+
+	void Update(){
+		Vector2 pos = new Vector2 (transform.position.x, transform.position.y);
+		Vector2 tar = new Vector2 (follower.transform.position.x, follower.transform.position.y);
+		if(Vector2.Distance(pos-offset, tar)>tollerance)
+		{
+			transform.position = new Vector3(transform.position.x + (follower.transform.position.x-(transform.position.x-offset.x))*speed*Time.deltaTime,
+			                                 transform.position.y + (follower.transform.position.y-(transform.position.y-offset.y))*speed*Time.deltaTime,
+			                                 transform.position.z);
+		}
+	}
+
+}
