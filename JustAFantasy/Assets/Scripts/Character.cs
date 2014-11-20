b96cc6aa2865643149104d79168ed58a3393f679
davide9 <bianchi_davide@hotmail.it>
2014-11-20 13:46:45 +0100

    Aggiunto script dati personaggio

---
+++ JustAFantasy/Assets/Scripts/Character.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+	public int lifePoints = 3;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
