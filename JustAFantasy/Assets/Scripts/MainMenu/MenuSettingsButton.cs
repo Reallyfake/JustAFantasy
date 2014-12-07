f6f1a130316c9b17b415028613732c19827784a7
Davide Bianchi <bianchi_davide@hotmail.it>
2014-12-07 15:57:22 +0100

    MainMenu

---
+++ JustAFantasy/Assets/Scripts/MainMenu/MenuSettingsButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSettingsButton : MonoBehaviour {
+
+	public GameObject cube;
+
+	void OnMouseUp(){
+		StartCoroutine (Rotate());
+		}
+	
+	IEnumerator Rotate() {
+		for (int f = 0; f < 55; f++) {
+			cube.transform.Rotate(0f,1f,0f);
+			yield return new WaitForSeconds(.03f);
+		}
+	}
+}
---
+++ JustAFantasy/Assets/Scripts/MainMenu/OnMouseColor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnMouseColor : MonoBehaviour {
+
+	void OnMouseEnter() {
+		renderer.material.color = Color.red;
+	}
+
+	void OnMouseExit() {
+		renderer.material.color = Color.white;
+	}
+}
