63d98bba8a3a65000e55527d2b7291b6a99d86ec
Davide Bianchi <bianchi_davide@hotmail.it>
2014-12-08 19:24:59 +0100

    Fix to WeaponWheel show/hide

--- JustAFantasy/Assets/Scripts/GameController.cs
+++ JustAFantasy/Assets/Scripts/GameController.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+	private GameObject WeaponWheel;
+	private bool is_running = false;
 	// Use this for initialization
 	void Start () {
-	
+		WeaponWheel = transform.GetChild (0).gameObject;
 	}
 	
 	// Update is called once per frame
@@ -15,10 +17,25 @@ public class GameController : MonoBehaviour {
 						transform.GetChild (1).gameObject.SetActive (true);
 				}
 		if (Input.GetKeyDown (KeyCode.W)) {
-			transform.GetChild(0).gameObject.transform.position = new Vector2(0,0);
+			if(is_running)
+				StopCoroutine("DragWWTo");
+			StartCoroutine("DragWWTo", 0f);
 				}
 		if (Input.GetKeyUp (KeyCode.W)) {
-			transform.GetChild(0).gameObject.transform.position = new Vector2(0,1);
+			if(is_running)
+				StopCoroutine("DragWWTo");
+			StartCoroutine("DragWWTo",1f);
 		}
+	}
+
+	IEnumerator DragWWTo(float newPos) {
+		is_running = true;
+		float step = (newPos - WeaponWheel.transform.position.y) / 20;
+		for (int f = 0; f < 20; f++) {
+			WeaponWheel.transform.position = new Vector2(0, WeaponWheel.transform.position.y + step);
+			yield return new WaitForSeconds(.005f);
+		}
+		WeaponWheel.transform.position = new Vector2 (0, newPos);
+		is_running = false;
 	}
 }
