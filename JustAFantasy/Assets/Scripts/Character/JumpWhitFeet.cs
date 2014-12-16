b3ec95ec1713d64818850314f07ca246803b0f81
davide9 <bianchi_davide@hotmail.it>
2014-12-16 10:07:35 +0100

    Updated prefabs

--- JustAFantasy/Assets/Scripts/Character/JumpWhitFeet.cs
+++ JustAFantasy/Assets/Scripts/Character/JumpWhitFeet.cs
@@ -38,7 +38,7 @@ public class JumpWhitFeet : MonoBehaviour {
     IEnumerator denyJump()
     {
         isRunning = true;
-        yield return new WaitForSeconds(.2f);
+        yield return new WaitForSeconds(.1f);
         (transform.parent.GetComponent<MovableWASD>() as MovableWASD).denyJump();
         isRunning = false;
     }
