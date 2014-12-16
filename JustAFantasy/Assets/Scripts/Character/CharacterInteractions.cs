d894044a499f4b0945f445b1374741d7c36171d8
davide9 <bianchi_davide@hotmail.it>
2014-12-16 17:25:40 +0100

    Working Spikes

--- JustAFantasy/Assets/Scripts/Character/CharacterInteractions.cs
+++ JustAFantasy/Assets/Scripts/Character/CharacterInteractions.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class CharacterInteractions : MonoBehaviour {
 
-	public float lastHitTime;
 	public float hitRatio;
 
     void OnTriggerEnter2D(Collider2D other)
@@ -15,26 +14,15 @@ public class CharacterInteractions : MonoBehaviour {
             if (transform.parent != null)
                 transform.parent.SendMessage("OnPlayerHit", dmg, SendMessageOptions.DontRequireReceiver);
         }
-
-		if (other.gameObject.tag.Equals("Spike"))
-		{
-			Vector2 dir = -(other.transform.position - transform.position);
-			Rigidbody2D rb= this.GetComponent<Rigidbody2D>() as Rigidbody2D;
-			rb.AddForce(dir*5f,ForceMode2D.Impulse);
-			if (transform.parent != null){
-				lastHitTime = Time.time + hitRatio;
-				transform.parent.SendMessage("OnPlayerHit", 1, SendMessageOptions.DontRequireReceiver);
-			}
-		}
     }
 
-	void OnCollisionStay2D(Collision2D other)
+	void OnCollisionEnter2D(Collision2D other)
 	{
 		if (other.gameObject.tag.Equals("Spike"))
 		{
-			if (transform.parent != null && Time.time > lastHitTime){
-				lastHitTime = Time.time + hitRatio;
+			if (transform.parent){
 				transform.parent.SendMessage("OnPlayerHit", 1, SendMessageOptions.DontRequireReceiver);
+                gameObject.SendMessage("SpikeHit", other.transform.position, SendMessageOptions.DontRequireReceiver);
 			}
 		}
 	}
--- JustAFantasy/Assets/Scripts/Character/MovableWASD.cs
+++ JustAFantasy/Assets/Scripts/Character/MovableWASD.cs
@@ -12,6 +12,7 @@ public class MovableWASD : MonoBehaviour {
     private GameObject feet;
     private Animator anim;
     private bool lookAhead = true;
+    private bool canMove = true;
 
     public void allowJump()
     {
@@ -51,21 +52,21 @@ public class MovableWASD : MonoBehaviour {
 	// Update is called once per frame
     void Update()
     {
-        if (isForward())
+        if (isForward() && canMove)
         {
             rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
             transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
         else
         {
-            if (isBackward())
+            if (isBackward() && canMove)
             {
                 rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
                 transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * -1f, transform.localScale.y, transform.localScale.z);
             }
             else
             {
-                if (!isForward() && !isBackward())
+                if (!isForward() && !isBackward() && canMove)
                 {
                     rb.velocity = new Vector2(0, rb.velocity.y);
                 }
@@ -92,5 +93,18 @@ public class MovableWASD : MonoBehaviour {
         grounded = Physics2D.OverlapCircle(feet.transform.position, .5f, WhatIsGround);
         if (anim != null)
             anim.SetBool("inAir", !grounded);
+    }
+
+    void SpikeHit(Vector3 spikePos)
+    {
+        rb.AddForce((transform.position - spikePos)*5, ForceMode2D.Impulse);
+        StartCoroutine(knockBack(.2f));
+    }
+
+    IEnumerator knockBack(float s)
+    {
+        canMove = false;
+        yield return new WaitForSeconds(s);
+        canMove = true;
     }
 }
