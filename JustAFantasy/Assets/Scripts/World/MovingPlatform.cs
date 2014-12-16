5237b43d4a9a7cbacbd17bdeb6e20f41c1a31c76
Nck19 <nicholascolombo@MacBook-Pro-di-Nicholas-Colombo.local>
2014-12-16 15:49:15 +0100

    Immagini cambiate, aggiunti script piattaforme
    - mancano i button…

---
+++ JustAFantasy/Assets/Scripts/World/MovingPlatform.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+	Transform tf = null;
+	
+	public bool isMovingVertical = false;
+	public bool isMovingHorizontal = false;
+	public bool eventBased = false;
+	public bool startMoving = false;
+	public float speed = 2f;
+	public float pingLength = 1f;
+	public float pongTime = 3f;
+	public float height = 0.01f;
+	public float width = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+		tf = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (eventBased == true) {
+			//una variablie specifica x ogni elemento che si muove ad evento
+			if (isMovingVertical == true && startMoving == true) {
+				this.MovingVertical ();
+			}
+			if (isMovingHorizontal == true && startMoving == true) {
+				this.MovingHorizontal ();
+			}
+		} else {
+			if (isMovingVertical == true) {
+				this.MovingVertical ();
+			}
+			if (isMovingHorizontal == true) {
+				this.MovingHorizontal ();
+			}
+		}
+	}
+
+	void MovingVertical(){
+		tf.position = tf.position + Vector3.Lerp(Vector3.down*height, Vector3.up*height ,
+		                                         Mathf.PingPong(Time.time / pongTime, pingLength));
+	}
+
+	void MovingHorizontal(){
+		tf.position = tf.position + Vector3.Lerp(Vector3.left*width, Vector3.right*width ,
+		                                         Mathf.PingPong(Time.time / pongTime, pingLength));
+	}
+}
---
+++ JustAFantasy/Assets/Scripts/World/Smashing.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Smashing : MonoBehaviour {
+
+	Transform tf = null;
+
+	public GameObject explosion;
+
+	public bool isMoving = false;
+	public bool eventBased = false;
+	public bool startMoving = false;
+	public float initialPos = 2f;
+	public float speedDown = 4f;
+	public float speedUp = 2f;
+	public float nextUp, nextDown;
+
+	private bool earthLevel = false;
+
+	
+	// Use this for initialization
+	void Start () {
+		tf = transform;
+//		tf.position.y = initialPos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//check su avriablie globale modificata da collisione con player
+		if (eventBased == true) {
+			if (isMoving == true && startMoving == true) {
+				SmashingIt ();
+			}
+		} else {
+			if (isMoving == true) {
+				SmashingIt ();
+			}
+		}
+
+//		tf.position = new Vector2 (tf.position.x, initialPos);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "player") {
+			Instantiate (explosion, transform.position, transform.rotation);
+			Destroy (other.gameObject);
+		}
+		else{ //collisione col terreno
+			nextUp= Time.time + nextDown; 
+			earthLevel = true;
+		}
+//	new Vector2 (tf.position.x, initialPos);
+	}
+
+	void SmashingIt(){
+		if (!earthLevel){
+			tf.position = tf.position + Vector3.down*speedDown*Time.deltaTime;
+		} 
+		else{
+			if (Time.time > nextUp){
+				tf.position = tf.position + Vector3.up*speedUp*Time.deltaTime;
+				if(tf.position.y > initialPos){
+					earthLevel = false;
+				}
+			}
+		}
+	}
+
+}
