94429d9be67a062830155c98df1c9f3af0839ed7
Reallyfake <reallyfake17@gmail.com>
2014-12-18 22:51:31 +0100

    Esempio di menù mappa

---
+++ JustAFantasy/Assets/Scripts/WorldMapNumber.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldMapNumber : MonoBehaviour {
+
+	public string levelName;
+
+	void OnMouseUp(){
+		Application.LoadLevel(levelName);
+	}
+
+}
