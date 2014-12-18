9670c9adfe674af7cd7ce734338d5b64ebd455d4
Reallyfake <reallyfake17@gmail.com>
2014-12-18 23:28:03 +0100

    Aggiunto CessPoint

---
+++ JustAFantasy/Assets/Scripts/CessPoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CessPoint : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Player") {
+			this.GetComponent<Animator>().SetTrigger("touched");
+			Debug.Log ("touchè");
+		}
+	}
+
+}
