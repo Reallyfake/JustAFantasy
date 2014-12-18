c5d90a6d220d319043cb261e7023882b80d77ac9
Reallyfake <reallyfake17@gmail.com>
2014-12-18 15:55:32 +0100

    Aggiunto InstaDeath

---
+++ JustAFantasy/Assets/Scripts/World/InstaDeath.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstaDeath : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag.Equals("Player"))
+		{
+			other.gameObject.SendMessage("Die",SendMessageOptions.DontRequireReceiver);
+		}
+	}
+
+}
