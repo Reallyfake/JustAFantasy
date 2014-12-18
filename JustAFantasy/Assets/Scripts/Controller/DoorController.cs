7119abec1d8c2bd922d9a07262d69c8402140dec
Reallyfake <reallyfake17@gmail.com>
2014-12-18 23:54:55 +0100

    Porta distruttibile a fucilate

---
+++ JustAFantasy/Assets/Scripts/Controller/DoorController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : jfEnemyController {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
