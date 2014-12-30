071866c9eb39fd1045a9998e3859d15fb6097825
Reallyfake <reallyfake17@gmail.com>
2014-12-30 21:36:39 +0100

    sistemata la leva nel tutorial

---
+++ JustAFantasy/Assets/Scripts/World/SimpleLever.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleLever : MonoBehaviour {
+
+    public GameObject target;
+    public bool onRight = false;
+    public string actionRight = "Active";
+	public string actionLeft = "DeActive";
+
+    public Sprite rightLever;
+    public Sprite leftLever;
+	public LayerMask lay;
+
+	// Use this for initialization
+	void Start () {
+        if (onRight)
+            (GetComponent<SpriteRenderer>() as SpriteRenderer).sprite = rightLever;
+        else
+            (GetComponent<SpriteRenderer>() as SpriteRenderer).sprite = leftLever;
+	}
+
+	void Update(){
+   
+		if (Physics2D.OverlapCircle(transform.position,0.6f,lay) && Input.GetKeyDown(KeyCode.D))
+        {
+
+			if (onRight){
+	            onRight = false;
+	            if (target != null)
+	                target.SendMessage(actionLeft, SendMessageOptions.DontRequireReceiver);
+	            (GetComponent<SpriteRenderer>() as SpriteRenderer).sprite = leftLever;
+			} else {
+				onRight = true;
+				if (target != null)
+					target.SendMessage(actionRight, SendMessageOptions.DontRequireReceiver);
+				(GetComponent<SpriteRenderer>() as SpriteRenderer).sprite = rightLever;
+			}
+        }
+    }
+}
