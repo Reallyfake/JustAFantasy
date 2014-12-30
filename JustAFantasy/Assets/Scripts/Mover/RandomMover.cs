368d8cb7751d2a6d209870467d87d44b565883ac
Reallyfake <reallyfake17@gmail.com>
2014-12-30 16:09:21 +0100

    Creata arma WTFireworks

---
+++ JustAFantasy/Assets/Scripts/Mover/RandomMover.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMover : MonoBehaviour {
+
+	private Transform tf;
+	private Rigidbody2D rb;
+	public float Xprobability=0.5f;
+	public float Yprobability=0.5f;
+	private float Xversus,Yversus;
+	public float Xspeed=5f;
+	public float Yspeed=5f;
+
+	// Use this for initialization
+	void Start () {
+		tf= this.GetComponent<Transform>() as Transform;
+		rb= this.GetComponent<Rigidbody2D>() as Rigidbody2D;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Random.Range(0f,1f) < Xprobability){
+			Xversus = 1f;
+		} else {
+			Xversus = -1f;
+		}
+
+		if (Random.Range(0f,1f) < Yprobability){
+			Yversus = 1f;
+		} else {
+			Yversus = -1f;
+		}
+
+		rb.velocity += Vector2.right*Xspeed*Time.deltaTime*Xversus;
+		rb.velocity += Vector2.up*Yspeed*Time.deltaTime*Yversus;
+
+		float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+		tf.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+
+
+	
+	}
+}
---
+++ JustAFantasy/Assets/Scripts/Weapons/WeaponWTFireworks.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponWTFireworks : jfWeapon {
+
+    private GameObject shotSpawn;
+    public GameObject shoot;
+
+    private float lastShotTime = 0f;
+    
+
+	// Use this for initialization
+	void Start () {
+        base.OnStart();
+        shotSpawn = transform.GetChild(0).gameObject;
+	}
+
+    public override void OpenFire()
+    {
+        if((Time.time - lastShotTime) >= fireRatio && (Ammo>0 || Ammo == -1))
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                GameObject o = Instantiate(shoot, transform.position, Quaternion.AngleAxis(90, Vector3.forward)) as GameObject;
+            }
+            else
+            {
+                if (anim != null)
+                    anim.SetTrigger("isShooting");
+                GameObject o = Instantiate(shoot, transform.position, transform.rotation) as GameObject;
+                if (transform.lossyScale.x < 0)
+                    (o.GetComponent<RandomMover>() as RandomMover).Xspeed *= -1;
+            }
+            lastShotTime = Time.time;
+            if(Ammo != -1)
+                Ammo--;
+        }
+    }
+}
