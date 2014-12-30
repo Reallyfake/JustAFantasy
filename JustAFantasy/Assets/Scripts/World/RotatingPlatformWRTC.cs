677d6132e39f86177b522eedaad5288fc300c5f0
Davide Bianchi <bianchi_davide@hotmail.it>
2014-12-30 18:50:21 +0100

    NEW RotatingPlatoformWRTC e scena test livello

---
+++ JustAFantasy/Assets/Scripts/World/RotatingPlatformWRTC.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingPlatformWRTC : MonoBehaviour {
+
+    public float speed = 0.5f; // u/s
+    public float radius = 1.0f;
+    public Vector2 center = new Vector2(0, 0);
+    public bool clockwise = true;
+    public float startingAngle = 270.0f;
+    public bool rotating = true;
+
+    private float rad;
+	// Use this for initialization
+	void Start () {
+        startingAngle = startingAngle % 360;
+        rad = (startingAngle / 180.0f) * Mathf.PI;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!clockwise)
+            rad += speed * Time.deltaTime / radius;
+        else
+            rad -= speed * Time.deltaTime / radius;
+        rad = rad % (2 * Mathf.PI);
+        Vector2 pos = new Vector2(Mathf.Cos(rad) * radius, Mathf.Sin(rad) * radius);
+        transform.localPosition = center + pos;
+	}
+}
