e545dba046b32f7c5e6cd35478f1a3ad5c89031c
Reallyfake <reallyfake17@gmail.com>
2015-01-02 17:49:18 +0100

    Script per animare i collider

---
+++ JustAFantasy/Assets/Scripts/ColliderFollowAnimation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderFollowAnimation : MonoBehaviour {
+
+	SpriteRenderer sp;
+	BoxCollider2D bc;
+	CircleCollider2D cc;
+
+	// Use this for initialization
+	void Start () {
+		sp=this.GetComponent<SpriteRenderer>() as SpriteRenderer;
+		bc=this.GetComponent<BoxCollider2D>() as BoxCollider2D;
+		cc=this.GetComponent<CircleCollider2D>() as CircleCollider2D;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (bc!=null){
+			bc.size = sp.sprite.bounds.size;
+			bc.center = sp.sprite.bounds.center;
+		}
+		if (cc!=null){
+			cc.radius = Mathf.Min(sp.sprite.bounds.size.x,sp.sprite.bounds.size.y)/2f;
+			cc.center = sp.sprite.bounds.center;
+		}
+	}
+}
---
+++ JustAFantasy/Assets/Scripts/EdgeColliderFollowAnimation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EdgeColliderFollowAnimation : MonoBehaviour {
+
+	SpriteRenderer sp;
+	//BoxCollider2D bc;
+	EdgeCollider2D ed;
+
+	// Use this for initialization
+	void Start () {
+		sp=this.GetComponent<SpriteRenderer>() as SpriteRenderer;
+		//bc=this.GetComponent<BoxCollider2D>() as BoxCollider2D;
+		ed=this.GetComponent<EdgeCollider2D>() as EdgeCollider2D;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 spSize = sp.bounds.size;
+		Vector3 edSize = ed.bounds.size;
+		float xRatio = spSize.x / edSize.x;
+		float yRatio = spSize.y / edSize.y;
+		int n =ed.pointCount;
+		Vector2[] newPoints = new Vector2[n];
+		for (int i=0; i< ed.pointCount; i++){
+			Debug.Log(ed.points[i]);
+			float newX = ed.points[i].x*xRatio;
+			float newY = ed.points[i].y*yRatio;
+			Debug.Log(newX+" "+newY);
+			newPoints[i]=new Vector2 (newX,newY);
+			Debug.Log(ed.points[i] +" nuovo");
+		}
+		ed.points = newPoints;
+	}
+}
