9ab7f110986a16e567608dc6ed2622ded04005be
Davide Bianchi <bianchi_davide@hotmail.it>
2015-01-03 21:36:46 +0100

    Updated HUD, new LifeBar system

--- JustAFantasy/Assets/Scripts/HUD/LifeBar.cs
+++ JustAFantasy/Assets/Scripts/HUD/LifeBar.cs
@@ -4,10 +4,28 @@ using System.Collections;
 public class LifeBar : MonoBehaviour {
 
 	private int currentLife = 5;
-	private const int maxLife = 10;
+	public const int maxLife = 10;
+
+    public Texture2D LifeThumb;
+
+    private GameObject[] LifePoints;
 
     void Start()
     {
+        //transform.position = new Vector3(0, 0, 0);
+        LifePoints = new GameObject[maxLife];
+        GameObject o;
+        for (int i = 0; i < maxLife; i++)
+        {
+            o = new GameObject("LifePoint" + i);
+            o.transform.parent = gameObject.transform;
+            o.AddComponent<GUITexture>();
+            o.transform.position = new Vector3(0.07f + 0.035f * i, 0.95f, 0f);
+            o.transform.localScale = new Vector3(0, 0, 1);
+            o.guiTexture.texture = LifeThumb;
+            o.guiTexture.pixelInset = new Rect(-19f, -19f, 38f, 38f);
+            LifePoints[i] = o;
+        }
         setLifePoints(currentLife);
     }
 
@@ -32,10 +50,10 @@ public class LifeBar : MonoBehaviour {
             n = maxLife;
         currentLife = n;
 		for (int i=0; i<n; i++) {
-			(transform.GetChild(i+1).gameObject.GetComponent<GUITexture> ()as GUITexture).enabled = true;
+			(LifePoints[i].GetComponent<GUITexture> ()as GUITexture).enabled = true;
 				}
 		for (int i=n; i<maxLife; i++) {
-			(transform.GetChild(i+1).gameObject.GetComponent<GUITexture> ()as GUITexture).enabled = false;
+            (LifePoints[i].GetComponent<GUITexture>() as GUITexture).enabled = false;
 				}
 	}
 }
