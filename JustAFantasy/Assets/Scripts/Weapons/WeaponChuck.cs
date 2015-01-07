1a898950ba4568e2945be1a000109cff4d92d169
Reallyfake <reallyfake17@gmail.com>
2015-01-07 18:29:11 +0100

    Music Commit of Death

--- JustAFantasy/Assets/Scripts/Weapons/WeaponChuck.cs
+++ JustAFantasy/Assets/Scripts/Weapons/WeaponChuck.cs
@@ -7,18 +7,24 @@ public class WeaponChuck : jfWeapon {
     public GameObject shoot;
 
     private float lastShotTime = 0f;
+
+	private AudioSource audioWeapon;
     
 
 	// Use this for initialization
 	void Start () {
         base.OnStart();
         shotSpawn = transform.GetChild(0).gameObject;
+		audioWeapon = this.GetComponent<AudioSource>() as AudioSource;
 	}
 
     public override void OpenFire()
     {
         if((Time.time - lastShotTime) >= fireRatio && (Ammo>0 || Ammo == -1))
         {
+			if (audioWeapon != null){
+				audioWeapon.Play();
+			}
             
                 if (anim != null)
                     anim.SetTrigger("isShooting");
