65f337d947ef9ae6f44f6517adf35ab765a8ba44
davide9 <bianchi_davide@hotmail.it>
2015-01-12 16:15:49 +0100

    Added slideObject

---
+++ JustAFantasy/Assets/Scripts/World/SlideObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlideObject : MonoBehaviour {
+
+	public Vector2 direction;
+
+	// Use this for initialization
+	void onActive(){
+		transform.Translate (direction);
+		}
+}
