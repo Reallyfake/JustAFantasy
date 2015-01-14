3727bc9f1f3d5c6d03583d7aa6b88fd632c0c7aa
Reallyfake <reallyfake17@gmail.com>
2015-01-14 14:09:49 +0100

    Il drago sputa fuoco

---
+++ JustAFantasy/Assets/Scripts/RotatingAll.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingAll : MonoBehaviour {
+
+	Transform tf;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		tf = this.GetComponent<Transform>() as Transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tf.Rotate(Vector3.forward,speed*Time.deltaTime);	
+	}
+}
