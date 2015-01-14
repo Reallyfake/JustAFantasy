fc590269b8658e605ba1f20295f6201ddc7aa53a
Reallyfake <reallyfake17@gmail.com>
2015-01-14 17:43:26 +0100

    Creati i settings

---
+++ JustAFantasy/Assets/Scripts/SettingsAudio.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsAudio : MonoBehaviour {
+
+	public int mode;
+
+	void OnMouseUp(){
+		switch (mode){
+		case 1 : AudioSettings.speakerMode = AudioSpeakerMode.Mono;
+			break;
+		case 2 : AudioSettings.speakerMode = AudioSpeakerMode.Stereo;
+			break;
+		case 3 : AudioSettings.speakerMode = AudioSpeakerMode.Quad;
+			break;
+		default : AudioSettings.speakerMode = AudioSpeakerMode.Surround;
+			break;
+		}
+		if (GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>() != null){
+			GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>().Play();
+		}
+	}
+}
---
+++ JustAFantasy/Assets/Scripts/SettingsQuality.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsQuality : MonoBehaviour {
+
+	public int mode;
+	
+	void OnMouseUp(){
+		QualitySettings.SetQualityLevel(mode);
+	}
+}
