371067f1dbd142815165d7ebf2697edf54a12cea
Reallyfake <reallyfake17@gmail.com>
2015-01-15 20:50:56 +0100

    Aggiunto sfondo Tutorial

---
+++ JustAFantasy/Assets/Scripts/AlphaBackGroundTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlphaBackGroundTest : MonoBehaviour {
+
+	SpriteRenderer sp;
+	Color c;
+	public float v;
+	// Use this for initialization
+	void Start () {
+		sp= this.GetComponent<SpriteRenderer>() as SpriteRenderer;
+		c=sp.material.color;
+		c.a=v;
+		sp.material.color=c;
+	}
+	
+
+}
