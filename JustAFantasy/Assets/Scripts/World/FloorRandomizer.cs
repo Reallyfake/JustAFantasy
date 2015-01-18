af24c0c0b3e7fe5fa24a9635839cdd5dce0bbea4
Davide Bianchi <bianchi_davide@hotmail.it>
2015-01-18 16:51:40 +0100

    FloorRandomizer

---
+++ JustAFantasy/Assets/Scripts/World/FloorRandomizer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorRandomizer : MonoBehaviour {
+
+    public Texture2D[] Sprites = new Texture2D[8];
+
+	// Use this for initialization
+	void Start () {
+        gameObject.renderer.material.mainTexture = Sprites[Random.Range(0, 4)];
+        
+	}
+	
+}
