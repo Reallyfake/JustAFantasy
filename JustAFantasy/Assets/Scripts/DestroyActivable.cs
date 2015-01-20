3e5373ebe7bb873f82cd870db8bc810466d1f785
Reallyfake <reallyfake17@gmail.com>
2015-01-20 16:41:33 +0100

    DestroyActivable per tutorial

---
+++ JustAFantasy/Assets/Scripts/DestroyActivable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyActivable : MonoBehaviour {
+
+	public GameObject[] destructable;
+	int i;
+	bool killMe;
+	
+	// Update is called once per frame
+	void Update () {
+
+		killMe = true;
+		for (i=0;i<destructable.Length;i++){
+			if (destructable[i] != null){
+				killMe = false;
+			}
+		}	
+		if (killMe){
+			Destroy(gameObject);
+		}
+	}
+}
