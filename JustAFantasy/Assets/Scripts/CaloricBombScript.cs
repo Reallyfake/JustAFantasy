74998e3f2f12d129cae0d36734aad5fefb3f99bf
Reallyfake <reallyfake17@gmail.com>
2015-01-22 23:13:41 +0100

    Mega commit of the death

--- JustAFantasy/Assets/Scripts/CaloricBombScript.cs
+++ JustAFantasy/Assets/Scripts/CaloricBombScript.cs
@@ -1,36 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ShotController : MonoBehaviour {
+public class CaloricBombScript : MonoBehaviour {
 
 	public GameObject explosion;
 	// enemy Health component
 	Energy enemyHealth;
-
+	
 	// damage of one shot
 	public int damage;
-    public float lifeTime = 10f;
-
+	public float lifeTime = 10f;
+	
 	//should the shot remain in the scene?
 	public bool resist;
-    
+	
 	void OnTriggerEnter2D (Collider2D other)
 	{
-        
+		
 		if (other.tag == "Floor" && !resist) {
 			Destroy(gameObject);
 		}
 	}
-    
-    void Update()
-    {
-        lifeTime -= Time.deltaTime;
-        if (lifeTime < 0)
-            Destroy(gameObject);
-    }
+	
+	void Update()
+	{
+		lifeTime -= Time.deltaTime;
+		if (lifeTime < 0)
+			Destroy(gameObject);
+	}
 
 	void OnDestroy() {
-		if (explosion != null)
-			Instantiate(explosion,transform.position, transform.rotation);
+		Instantiate(explosion,transform.position, transform.rotation);
 	}
 }
